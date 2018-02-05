7dc7cfcc952928038f22322059ac4991419976b8
Natalie Contreras <natalie.isabel.contreras.a@gmail.com>
2018-02-05 13:06:27 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen"
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+
+var cards=["queen", "queen", "king", "king"];
+
+var cardsInPlay=[];
+
+var cardOne=[0];
+cardsInPlay.push('cardOne');
+
+console.log("User flipped queen");
+
+var cardTwo=[2];
+cardsInPlay.push('cardTwo');
